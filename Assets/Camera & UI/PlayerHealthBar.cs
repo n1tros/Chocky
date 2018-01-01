3354a2d7238e52784c3d8ea2ef4ec77b16626292
Unknown <blasmafreed@hotmail.com>
2018-01-01 16:12:56 +0000

    Player Health bar

---
+++ Assets/Camera & UI/PlayerHealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class PlayerHealthBar : MonoBehaviour
+{
+
+    RawImage _healthBarRawImage;
+    [SerializeField] AgentController _player;
+    Health _playerHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        _healthBarRawImage = GetComponent<RawImage>();
+        _playerHealth = _player.GetComponent<Health>();
+    }
+
+    public void HealthUpdate(float currentHealth)
+    {
+        float xValue = -(currentHealth / 2f) - 0.5f;
+        _healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
--- Assets/Health.cs
+++ Assets/Health.cs
@@ -3,8 +3,9 @@
 public class Health : MonoBehaviour
 {
     [SerializeField] float _currentHealth = 100f;
-    //[SerializeField] float _maxHealth = 100f;
+    [SerializeField] float _maxHealth = 100f;
     [SerializeField] BoxCollider2D _hotBoxCollider;
+    [SerializeField] PlayerHealthBar _healthBarObject;
 
     private AgentController _agent = null;
 
@@ -14,10 +15,29 @@ public class Health : MonoBehaviour
         set
         {
             if (value <= 0)
+            {
+                _currentHealth = 0;
                 Death();
+            }
             else
                 _currentHealth = value;
+
+            UpdateHealthBar();
         }
+    }
+
+    public float HealthAsPercentage
+    {
+        get
+        {
+            return _currentHealth / _maxHealth;
+        }
+
+    }
+
+    public void UpdateHealthBar()
+    {
+        _healthBarObject.HealthUpdate(HealthAsPercentage);
     }
 
     private void Death()
